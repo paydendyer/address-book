cb4fd5b1cadeed50f9832e178a660a6e0afb5518
Payden Dyer <63562333+paydendyer@users.noreply.github.com>
2022-04-03 18:36:00 -0500

    Added AddressBook Constructor

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// Business Logic for AddressBook
+function AddressBook() {
+  this.contacts = {};
+}
+//Business Logic for Contacts
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+};
